7bee475edf2fef98068c7272dc735c4bfad0ec2b
Francis <158580035+Francisred@users.noreply.github.com>
2024-10-03 16:44:35 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+import App from "./js/App.js";
+
+const monApp = new App();
+
+// Your code here
+monApp.createCanvas(window.innerWidth, window.innerHeight);
+
+monApp.createGrid();
+document.body.style.backgroundColor = "black";
