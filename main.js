22b2dc5277c24d37ef8c188551393fbc349fedab
Francis <158580035+Francisred@users.noreply.github.com>
2024-10-03 16:52:30 +0100

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-import App from "./js/App.js";
-
-const monApp = new App();
-
-// Your code here
-monApp.createCanvas(window.innerWidth, window.innerHeight);
-
-monApp.createGrid();
-document.body.style.backgroundColor = "black";
